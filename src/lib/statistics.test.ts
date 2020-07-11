30805b9925065a29f706e27bf3464310e87229af
Tim Rice <timrice+timrtc@gmail.com>
2020-07-11 13:49:18 -0400

    feat(statistics): calculations for mean, median, std deviation and mode.

---
+++ src/lib/statistics.test.ts
@@ -0,0 +1,33 @@
+import * as SUT from './statistics';
+import dataset from '../data-1234.json';
+
+describe('statistics', () => {
+  let data: number[];
+
+  beforeAll(() => {
+    data = dataset.data;
+  });
+
+  it('should calculate the mean', () => {
+    const result = SUT.calculateMean(data);
+    expect(result).toBe(49.45705);
+  });
+
+  it('should calculate the median', () => {
+    const result = SUT.calculateMedian(data);
+    expect(result).toBe(49.0);
+  });
+
+  it('should calculate the standard deviation', () => {
+    const result = SUT.calculateStdDeviation(data);
+
+    // specification says result for this sample set should be 28.810315
+    // actual results confirmed here: https://www.mathsisfun.com/data/standard-deviation-calculator.html
+    expect(result).toBe(28.798639);
+  });
+
+  it('should calculate the mode', () => {
+    const result = SUT.calculateMode(data);
+    expect(result).toBe(77);
+  });
+});
---
+++ src/lib/statistics.ts
@@ -0,0 +1,126 @@
+type Counter = { [key: string]: number };
+type ModeResult = { mode: number; count: number };
+
+/**
+ * Standardize calculation results by truncating to 6 signifcant decimal places.
+ * TODO: consider returning raw result and standardizing in view component.
+ *
+ * @param {number} n
+ * @returns {number}
+ */
+function standardize(n: number): number {
+  return Number(n.toFixed(6));
+}
+
+/**
+ * Get the average value for a numeric array.
+ *
+ * @param {number[]} numbers
+ * @returns {number}
+ */
+function getAverage(numbers: number[]): number {
+  return numbers.reduce((sum, n) => sum + n) / numbers.length;
+}
+
+/**
+ * Calculate the mean value for the given set of numbers.
+ *
+ * @see https://en.wikipedia.org/wiki/Arithmetic_mean
+ * @export
+ * @param {number[]} data
+ * @returns {number}
+ */
+export function calculateMean(data: number[]): number {
+  const mean = getAverage(data);
+  return standardize(mean);
+}
+
+/**
+ * Calculate the median value for the given set of numbers.
+ *
+ * @see https://en.wikipedia.org/wiki/Median
+ * @export
+ * @param {number[]} data
+ * @returns {number}
+ */
+export function calculateMedian(data: number[]): number {
+  if (!data.length) return 0;
+
+  const sorted = data.sort((a, b) => a - b);
+  const middle = Math.floor(sorted.length / 2);
+
+  let median: number = sorted[middle];
+
+  if (sorted.length % 2 === 0) {
+    median = (median + sorted[middle - 1]) / 2;
+  }
+
+  return standardize(median);
+}
+
+/**
+ * Calculate the standard deviation for the given set of numbers.
+ *
+ * @see https://en.wikipedia.org/wiki/Standard_deviation
+ * @export
+ * @param {number[]} data
+ * @returns {number}
+ */
+export function calculateStdDeviation(data: number[]): number {
+  const avg = getAverage(data);
+
+  const deviations = data.map((n) => Math.pow(n - avg, 2));
+  const variance = getAverage(deviations);
+
+  const stdDeviation = Math.sqrt(variance);
+
+  return standardize(stdDeviation);
+}
+
+/**
+ * Calculate the mode for the given set of numbers.
+ *
+ * @see https://en.wikipedia.org/wiki/Mode_(statistics)
+ * @export
+ * @param {number[]} data
+ * @returns {number}
+ */
+export function calculateMode(data: number[]): number {
+  // get the number of occurrences for each number in the set.
+  const counter = data.reduce<Counter>((agg, n) => {
+    agg[n] = (agg[n] ?? 0) + n;
+    return agg;
+  }, {});
+
+  // holder for the final result.
+  let result: ModeResult | undefined;
+
+  // find the number with the highest frequency of occurrences.
+  Object.keys(counter).forEach((k) => {
+    const n = Number(k);
+
+    const current: ModeResult = {
+      mode: n,
+      count: counter[k],
+    };
+
+    if (!result) {
+      result = current;
+      return;
+    }
+
+    // initialize result by capturing the first value.
+    if (!result) {
+      const n = Number(k);
+      result = { mode: n, count: counter[n] };
+      return;
+    }
+
+    // compare current to previous best and replace if better.
+    if (current.count > result.count) {
+      result = current;
+    }
+  });
+
+  return result !== undefined ? standardize(result.mode) : 0;
+}
