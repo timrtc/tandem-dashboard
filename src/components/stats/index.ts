4dc1e22011e6542c5247b35a75d65519c24c57a6
Tim Rice <timrice+timrtc@gmail.com>
2020-07-12 13:21:43 -0400

    chore: removed unused components.

--- src/components/stats/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Mean } from './Mean';
-export { default as Median } from './Median';
-export { default as Mode } from './Mode';
-export { default as StandardDeviation } from './StandardDeviation';
